166e6b8c1311100e7c80a64cc2bf09d6fd751fb4
smoreyaks <bnixon.eng@gmail.com>
2023-09-22 12:19:20 +1000

    Formatted w/ Prettier

--- client/src/App.js
+++ client/src/App.js
@@ -18,33 +18,42 @@ import Admin from "scenes/admin";
 import Performance from "scenes/performance";
 
 function App() {
-  const mode = useSelector((state) => state.global.mode);
-  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
-  return (
-    <div className="app">
-      <BrowserRouter>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Routes>
-            <Route element={<Layout />}>
-              <Route path="/" element={<Navigate to="/dashboard" replace />} />
-              <Route path="/dashboard" element={<Dashboard />} />
-              <Route path="/products" element={<Products />} />
-              <Route path="/customers" element={<Customers />} />
-              <Route path="/transactions" element={<Transactions />} />
-              <Route path="/geography" element={<Geography />} />
-              <Route path="/overview" element={<Overview />} />
-              <Route path="/daily" element={<Daily />} />
-              <Route path="/monthly" element={<Monthly />} />
-              <Route path="/breakdown" element={<Breakdown />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/performance" element={<Performance />} />
-            </Route>
-          </Routes>
-        </ThemeProvider>
-      </BrowserRouter>
-    </div>
-  );
+    const mode = useSelector((state) => state.global.mode);
+    const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+    return (
+        <div className="app">
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Routes>
+                        <Route element={<Layout />}>
+                            <Route
+                                path="/"
+                                element={<Navigate to="/dashboard" replace />}
+                            />
+                            <Route path="/dashboard" element={<Dashboard />} />
+                            <Route path="/products" element={<Products />} />
+                            <Route path="/customers" element={<Customers />} />
+                            <Route
+                                path="/transactions"
+                                element={<Transactions />}
+                            />
+                            <Route path="/geography" element={<Geography />} />
+                            <Route path="/overview" element={<Overview />} />
+                            <Route path="/daily" element={<Daily />} />
+                            <Route path="/monthly" element={<Monthly />} />
+                            <Route path="/breakdown" element={<Breakdown />} />
+                            <Route path="/admin" element={<Admin />} />
+                            <Route
+                                path="/performance"
+                                element={<Performance />}
+                            />
+                        </Route>
+                    </Routes>
+                </ThemeProvider>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
